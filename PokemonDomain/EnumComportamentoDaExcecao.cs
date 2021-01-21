41e2900642fab1d09b58db1656222a19bb0b6b3e
Nouara Xavier <nouara.xavier@true.com.br>
2021-01-21 12:24:44 -0300

    setup project exceptions

---
+++ PokemonDomain/EnumComportamentoDaExcecao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonDomain
+{
+    public enum EnumComportamentoDaExcecao
+    {
+        deslogaUsuario = 0,
+        mantemEstado = 1,
+        estadoAnterior = 2,
+        estadoPosterior = 3,
+        reiniciaFluxo = 4
+    }
+}
--- PokemonExceptions/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PokemonExceptions
-{
-    public class Class1
-    {
-    }
-}
